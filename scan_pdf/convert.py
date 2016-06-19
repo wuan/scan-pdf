b3733dca5f6f4d1ae6d22c5da64bed2f3707d4ad
Andreas Würl <andi@tryb.de>
2016-06-19 22:40:03 +0200

    handle page rotation in duplex mode

--- scan_pdf/convert.py
+++ scan_pdf/convert.py
@@ -4,13 +4,14 @@ import subprocess
 
 logger = logging.getLogger(__name__)
 
+
 class Converter(object):
     page_file_suffix = '.pdf'
 
     def __init__(self, options):
         self.options = options
 
-    def convert(self, source_basename, source_suffix):
+    def convert(self, source_basename, source_suffix, flip=False):
         logger.info('convert %s', source_basename)
         args = ['convert']
 
@@ -18,6 +19,8 @@ class Converter(object):
         args += ['-depth', str(color_depth)]
         args += ['-density', str(self.options.resolution)]
         args += ['-compress', 'zip']
+        if flip:
+            args += ['-rotate', '180']
         args += [source_basename + source_suffix, source_basename + self.page_file_suffix]
 
         return subprocess.call(args)
