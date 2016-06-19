863122c01755d5997adca217bfdaa6570a9ae111
Andreas Würl <andi@tryb.de>
2016-06-19 21:21:44 +0200

    fixed output file name

--- scan_pdf/combine.py
+++ scan_pdf/combine.py
@@ -11,9 +11,9 @@ class Combiner(object):
         self.options = options
 
     def combine(self, page_file_names):
-        logger.info("combine %d files into %s", len(page_file_names), self.options.output_file_name)
+        output_file_name = self.options.output_file_name[0]
+        logger.info("combine %d files into %s", len(page_file_names), output_file_name)
         combine_args = ['pdftk']
         combine_args += page_file_names
-        output_file_name = self.options.output_file_names[0]
         combine_args += ['output', os.path.basename(output_file_name), 'compress']
         return subprocess.call(combine_args)
--- scan_pdf/convert.py
+++ scan_pdf/convert.py
@@ -14,7 +14,8 @@ class Converter(object):
         logger.info('convert %s', source_basename)
         args = ['convert']
 
-        args += ['-depth', str(self.options.color_depth)]
+        color_depth = self.options.color_depth if self.options.color_mode != 'bw' else 1
+        args += ['-depth', str(color_depth)]
         args += ['-density', str(self.options.resolution)]
         args += ['-compress', 'zip']
         args += [source_basename + source_suffix, source_basename + self.page_file_suffix]
