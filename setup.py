24d7627bbe003ee8896ed6898b9ebdc7d1bd37d1
Andreas Wuerl <andi@tryb.de>
2020-12-18 23:43:07 +0100

    use pdfunite instead of pdftk

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='scan-pdf',
-    version='0.1.36',
+    version='0.2.0',
     packages=find_packages(where='./src'),
     package_dir={'': 'src'},
     scripts=['src/scan-pdf'],
