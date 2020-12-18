65b7568d4314af41318742c05379ec044d413c92
Andreas Wuerl <andi@tryb.de>
2020-12-18 23:33:06 +0100

    make tempfolder acesssible

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='scan-pdf',
-    version='0.1.35',
+    version='0.1.36',
     packages=find_packages(where='./src'),
     package_dir={'': 'src'},
     scripts=['src/scan-pdf'],
