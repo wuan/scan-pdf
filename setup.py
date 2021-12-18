dd37da80a3de81ba30fd58ba43b487b60d776697
Andreas Wuerl <andi@tryb.de>
2021-12-18 12:52:47 +0100

    update version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='scan-pdf',
-    version='0.2.5',
+    version='0.2.6',
     packages=find_packages(where='./src', exclude='tests'),
     package_dir={'': 'src'},
     scripts=['src/scan-pdf'],
